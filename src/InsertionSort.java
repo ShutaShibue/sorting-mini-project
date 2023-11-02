a50984c42cff2bbc29e1b3bf8bafd8eaf5c95fa7
poko <shuta.shibue@gmail.com>
2023-11-02 14:51:28 -0500

    implemented insertionsort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -24,8 +24,7 @@ public class InsertionSort implements Sorter {
   /**
    * Create a sorter.
    */
-  InsertionSort() {
-  } // InsertionSort()
+  InsertionSort() {} // InsertionSort()
 
   // +---------+-----------------------------------------------------
   // | Methods |
@@ -33,6 +32,17 @@ public class InsertionSort implements Sorter {
 
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
-    // STUB
+    int j;
+    for (int i = 1; i < values.length; i++) {
+      j = i;
+      while ((j > 0) && (order.compare(values[j - 1], values[j])) > 0) {
+        // swap
+        T temp = values[j];
+        values[j] = values[j-1];
+        values[j-1] = temp;
+
+        j--;
+      }
+    }
   } // sort(T[], Comparator<? super T>
 } // class InsertionSort
