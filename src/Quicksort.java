d21b2b99586245678a8510eaf319f2347a0e2d25
poko <shuta.shibue@gmail.com>
2023-11-02 14:53:04 -0500

    implemented quicksort

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -24,8 +24,7 @@ public class Quicksort implements Sorter {
   /**
    * Create a sorter.
    */
-  Quicksort() {
-  } // Quicksort()
+  Quicksort() {} // Quicksort()
 
   // +---------+-----------------------------------------------------
   // | Methods |
@@ -33,6 +32,40 @@ public class Quicksort implements Sorter {
 
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
+    sort(values, 0, values.length-1, order);
     // STUB
   } // sort(T[], Comparator<? super T>
-} // class Quicksort
+
+   public <T> void sort(T[] values, int lb, int ub, Comparator<? super T> order) {
+    if(lb >= ub) return;
+    int pivotIndex = partition(values, lb, ub, order);
+    sort(values, lb, pivotIndex-1, order);
+    sort(values, pivotIndex+1, ub, order);
+    // STUB
+  } // sort(T[], Comparator<? super T>
+
+  
+
+  private <T> int partition(T[] values, int lb, int ub, Comparator<? super T> order) {
+    T pivot = values[ub];
+    int i = lb - 1;
+
+    for (int j = lb; j < ub; j++) {
+      if (order.compare(values[j], pivot) <= 0) {
+        i++;
+
+        //swap
+        T temp= values[i];
+        values[i] = values[j];
+        values[j] = temp;
+      }
+    }
+
+    //put pivot to the middle
+    T temp = values[i + 1];
+    values[i + 1] = values[ub];
+    values[ub] = temp;
+    return i + 1;
+  }
+}
+// class Quicksort
