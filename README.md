0708291fc5e80edafb3d8df42f5cae52c91f8f25
poko <shuta.shibue@gmail.com>
2023-11-02 21:57:16 -0500

    Edited Readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # sorting-mini-project
-As the name suggests, it's our mini-project for the sorting lab.
+
+## Author
+
+Shuta Shibue
+
+## Contents
+
+This project implements variety of sorting algorithms and users can choose one. All algorithms implements Sorter
+interface, so there will be no effect even if you choose to use different sorting.
+Algorithms implemented:
+
+- InsertionSort
+- MergeSort
+- QuickSort
+- ShutaShibueSort
+
+ShutaShibueSort is the combination of QuickSort and InsertionSort. It executes two rounds of Partition action in quicksort to move elements close to its final place, and then finish with insertionsort which should be faster with half-sorted array.
+
+## Resources
+
+Labs, readings, EBoards from classes when we learned each sorting medthod.  
+Did not use ChatGPT or other LLMs.
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * Sort using merge sort.
  *
- * @author Your Name Here
+ * @author Shuta Shibue
  */
 
 public class MergeSort implements Sorter {
@@ -24,15 +26,71 @@ public class MergeSort implements Sorter {
   /**
    * Create a sorter.
    */
-  MergeSort() {
-  } // MergeSort()
+  MergeSort() {} // MergeSort()
 
   // +---------+-----------------------------------------------------
   // | Methods |
   // +---------+
 
-  @Override
-  public <T> void sort(T[] values, Comparator<? super T> order) {
+
+  public <T> void sort(T[] vals, Comparator<? super T> comparator) {
+    sort(vals, comparator, 0, vals.length - 1);
     // STUB
   } // sort(T[], Comparator<? super T>
+
+  /**
+   * @param vals array
+   * @param comparator Comparator
+   * @param lo low end of index, inclusive
+   * @param mid midpoint
+   * @param hi hi end of index, inclusive
+   */
+  public <T> void sort(T[] vals, Comparator<? super T> comparator, int lo, int hi) {
+
+    if (hi > lo) {
+      int m = (lo + hi) / 2;
+      // sort left half and then right half
+      sort(vals, comparator, lo, m);
+      sort(vals, comparator, m + 1, hi);
+      merge(vals, lo, m, hi, comparator);
+    }
+
+  }
+
+  /**
+   * Merge the values from positions [lo..mid) and [mid..hi) back into the same part of the array.
+   *
+   * Preconditions: Each subarray is sorted accorting to comparator.
+   */
+  @SuppressWarnings("unchecked")
+  static <T> void merge(T[] vals, int lo, int mid, int hi, Comparator<? super T> comparator) {
+    // STUB
+    int i = 0;
+    int loi = lo;
+    int hii = mid;
+    T[] temp = vals.clone();
+
+    while (loi < mid && hii < hi) {
+      if (comparator.compare(temp[loi], temp[hii]) > 0) {
+        vals[i] = temp[hii];
+        hii++;
+        i++;
+      } else {
+        vals[i] = temp[loi];
+        loi++;
+        i++;
+      }
+    }
+    while (hii < hi) {
+      vals[i] = temp[hii];
+      i++;
+      hii++;
+    }
+    while (hii < hi) {
+      vals[i] = temp[loi];
+      i++;
+      loi++;
+    }
+  } // merge
+
 } // class MergeSort
